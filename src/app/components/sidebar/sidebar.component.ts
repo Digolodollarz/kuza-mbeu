d46326bf6ea8a1b7a6a2f3da46c355a2e4d23d31
Eddie <eddie@diggle.tech>
2018-07-20 19:57:48 +0200

    Add navigation links Upgrade from angular 1.7.4. Well it's done now, and it still works.

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -10,12 +10,21 @@ declare interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
     { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
+    { path: '/accommodation', title: 'Accommodation',  icon:'home', class: '' },
+    { path: '/food', title: 'Food',  icon:'fastfood', class: '' },
+    { path: '/transport', title: 'Transport',  icon:'local_taxi', class: '' },
+    { path: '/entertainment', title: 'Entertainment',  icon:'local_movies', class: '' },
+    { path: '/printing', title: 'printing',  icon:'print', class: '' },
+    { path: '/store', title: 'store',  icon:'store', class: '' },
+    { path: '/groups', title: 'groups',  icon:'group', class: '' },
+    { path: '/source', title: 'source',  icon:'local_library', class: '' },
+    { path: '/adverts', title: 'adverts',  icon:'attach_money', class: '' },
+    { path: '/events', title: 'events',  icon:'event', class: '' },
     { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
     { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
     { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
     { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
     { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
-    { path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
 ];
 
 @Component({

cd4b444b2462961dfba19ed4bd49ef937e05473b
Eddie <eddie@diggle.tech>
2018-07-22 13:58:42 +0200

    Enable offline mode

---
+++ src/app/coming-soon/coming-soon.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ComingSoonComponent} from './coming-soon.component';
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [
+        ComingSoonComponent,
+    ]
+})
+export class ComingSoonModule {
+}
---
+++ src/app/user-profile/login-provider.ts
@@ -0,0 +1,6 @@
+enum LoginProvider {
+    Google,
+    Facebook,
+    Twitter,
+    Email
+}
